edea1dbb039d4d2316684324119c874a19480093
123abcln <nizhniea@mail.uc.edu>
2026-07-15 12:36:49 -0400

    Testing countdown+up and .gitignore

---
+++ Expense-tracker/py Testing countdown+up.py
@@ -0,0 +1,21 @@
+
+print("Liza is going down :")
+Liza = 10
+while Liza >= 1:
+    print(Liza)
+    Liza = Liza - 1
+
+print("Liza has reached the bottom!")
+
+print("Liza is going up now:")
+Liza = 0
+while Liza <= 10:
+    print(Liza)
+    Liza = Liza + 1
+print("Liza has reached the top! Yay!")
+
+Sum = 0
+for Value_1 in range(1,6):
+    Sum = Sum + Value_1
+
+print(Sum)
